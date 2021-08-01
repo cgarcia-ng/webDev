3b944d43aa6fd760e8ee86915b1fc3ec83bc66f9
Carlos Garcia <carlos.garcia@nextgenpty.com>
2021-08-01 12:29:54 -0500

    Mapeo de URLs (Repaso)

--- src/myWeb/urls.py
+++ src/myWeb/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 # from myWeb.views import hello_world
-from .views import hello_world
+from .views import hello_world, practice1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello-world/', hello_world),
+    path('practice-1/', practice1)
 ]
--- src/myWeb/views.py
+++ src/myWeb/views.py
@@ -2,5 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return render(request, 'templates/index.html')
+    resultado = 4 + 5
+    context = {
+        'nombre': 'Carlos',
+        'suma': resultado
+    }
+    return render(request, 'templates/index.html', context)
     # return HttpResponse('<h1>Hola Mundo. Mi nombre es Carlos</h1>')
+
+def practice1(request):
+    return render(request, 'templates/practice-1.html')
